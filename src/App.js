1990f011a8c4132f27a76ea5e579b7e631cd549f
Arshdeep Singh <arsh22sahni@gmail.com>
2023-10-24 11:07:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
@@ -10,47 +10,69 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
-class App extends Component {
-  state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment().add(1, "days").toDate(),
-        title: "Some title",
-      },
-    ],
+const App = () => {
+  const [events, setEvents] = useState([
+    {
+      start: moment().add(0, "days").toDate(),
+      end: moment().add(0, "days").toDate(),
+      title: "Some title",
+    },
+  ]);
+
+  const onEventResize = (data) => {
+    const updatedEvents = [...events];
+
+    // Find the index of the existing object in the copied array
+    const index = updatedEvents.findIndex((event) => event === data.event);
+
+    if (index !== -1) {
+      // Update the properties of the existing object with the values from the updated object
+      updatedEvents[index] = {
+        ...updatedEvents[index],
+        start: data.start,
+        end: data.end,
+        title: data.event.title, // You can update the title if needed
+      };
+
+      // Update the events state with the modified array
+      setEvents(updatedEvents);
+    }
   };
 
-  onEventResize = (data) => {
-    const { start, end } = data;
+  const onEventDrop = (data) => {
+    const updatedEvents = [...events];
 
-    this.setState((state) => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: [...state.events] };
-    });
+    // Find the index of the existing object in the copied array
+    const index = updatedEvents.findIndex((event) => event === data.event);
+
+    if (index !== -1) {
+      // Update the properties of the existing object with the values from the updated object
+      updatedEvents[index] = {
+        ...updatedEvents[index],
+        start: data.start,
+        end: data.end,
+        title: data.event.title, // You can update the title if needed
+      };
+
+      // Update the events state with the modified array
+      setEvents(updatedEvents);
+    }
   };
 
-  onEventDrop = (data) => {
-    console.log(data);
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <DnDCalendar
-          defaultDate={moment().toDate()}
-          defaultView="month"
-          events={this.state.events}
-          localizer={localizer}
-          onEventDrop={this.onEventDrop}
-          onEventResize={this.onEventResize}
-          resizable
-          style={{ height: "100vh" }}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <DnDCalendar
+        defaultDate={moment().toDate()}
+        defaultView="month"
+        events={events}
+        localizer={localizer}
+        onEventDrop={onEventDrop}
+        onEventResize={onEventResize}
+        resizable
+        style={{ height: "100vh" }}
+      />
+    </div>
+  );
+};
 
 export default App;
